5fdf091d476683c2009f95e76b4e37f49f268309
David Sheets <sheets@alum.mit.edu>
2014-06-13 17:19:56 +0100

    opam installs run better with 1gb of ram

--- xen/mk_fs-release-fs.sh
+++ xen/mk_fs-release-fs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xen-create-image --hostname=fs-$1-$2 --memory=512mb --vcpus=2 --pygrub --dist=$1 --dhcp --dir=fs_images --fs=$2 --image="sparse" --genpass=0 --role=`pwd`/agent_role.sh --password=password
+xen-create-image --hostname=fs-$1-$2 --memory=1024mb --vcpus=2 --pygrub --dist=$1 --dhcp --dir=fs_images --fs=$2 --image="sparse" --genpass=0 --role=`pwd`/agent_role.sh --password=password
