28400568e8100b2822a9540bec374a0ea531c8cb
David Sheets <sheets@alum.mit.edu>
2014-06-19 15:30:49 +0100

    Delegate fs deps to fs, be more precise in build command

--- xen/install_fs_test.sh
+++ xen/install_fs_test.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -ex
 
 . /root/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
-apt-get install -y m4 make wget
-wget http://caml.inria.fr/pub/distrib/ocaml-4.01/ocaml-4.01.0.tar.gz
-tar xvzf ocaml-4.01.0.tar.gz
-opam remote add ocamllabs https://github.com/ocamllabs/opam-repo-dev.git
-opam install -y ocamlfind sha cppo unix-unistd
+apt-get install -y m4 make wget libffi-dev
 git clone https://bitbucket.org/thtuerk/lem.git
 cd lem
 git fetch origin fs:fs
@@ -19,4 +15,6 @@ git fetch origin posix:posix
 git checkout posix
 ln -s ../../lem src_ext/lem
 ln -s ../../ocaml-4.01.0 src_ext/ocaml
-make
+opam install -y ocamlfind
+./install_opam_deps.sh --install -y
+make src_ext build_spec fs_test
