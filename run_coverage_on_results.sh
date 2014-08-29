529f2ef9628a1658df5f3c6dd5cc1fe3f2f69fa9
Andrea <agiugliano@live.it>
2014-08-29 13:18:04 +0100

    add script to obtain coverage report

---
+++ run_coverage_on_results.sh
@@ -0,0 +1,121 @@
+#!/bin/bash -e
+
+# this script is intended to check posix trace results and produce
+# coverage testing of the specification
+#
+# Usage:
+#     myself directory_with_posix_results
+#
+# or for quiet mode
+#     myself -q directory_with_posix_results
+
+if [[ ! $@ || $1 == "-h" || $1 == "--help" ]]
+then
+    echo "Usage: run_coverage_on_results.sh [OPTION] ... [DIR]
+      
+      run_coverage_on_results.sh compares
+      the results of running a trace of file systems commands
+      against an implementation with the results expected
+      by the POSIX specification
+      
+      OPTIONS:
+      -m            uses make to build the posix and the checker executables
+      -a <ARCH>     architecture of the spec: it can be [posix,linux,mac_os_x]
+      -h,--help     show a message help
+      "
+    exit 0;
+fi;
+
+# adapt to fit the dirs in your installation
+check_command=`readlink -f ./src_ext/fs/fs_test/check.native`
+posix_command=`readlink -f ./src_ext/fs/fs_test/posix.native`
+
+echo "Using check command \"$check_command\""
+echo "Using posix command \"$posix_command\""
+
+
+# safety check: fs must have the Makefile_bisect
+if  [[ -f ../src_ext/fs/Makefile_bisect ]]
+then echo "Error: the specification cannot be instrumented,
+           no Makefile_bisect in src_ext/fs/
+           (change fs branch in add_bisect or add_bisect_clean_c)"
+     exit 1;
+fi;
+
+if [[ $1 == "-m" ]]
+then
+    shift
+    dir=`pwd .`
+    cd ./src_ext/fs/
+    make clean
+    make
+    make -f Makefile_bisect
+    cd $dir
+fi;
+
+architecture=posix
+if [[ $1 = "-a" ]]
+then
+    shift
+    #safety check on the architecture
+    if ! [[ $1 = "posix" || $1 = "linux" || $1 = "mac_os_x" ]]
+    then
+        echo "Wrong architecture: the only allowed are posix, linux or mac_os_x"
+    exit 1;
+    fi;
+    architecture=$1
+    shift
+fi;
+
+echo "Checker uses the $architecture architecture"
+
+# safety check on passed directory
+if ! [[ -d $1 ]]
+then
+    echo "Missing directory containing results
+          (Hint: it should be something like
+          artefacts/2014-08-27_artefacts/2014-08-27_WGf/ext4_loop/)"
+    exit 1;
+fi;
+
+results_dir=`readlink -f $1`
+coverage_dir=$results_dir/coverage
+#remove results of a previous run
+rm -rf $coverage_dir
+mkdir $coverage_dir
+
+for d in $results_dir/*; # TODO remove coverage/ from this list
+do
+    if [[ $d == $coverage_dir ]]
+    then
+        continue;
+    fi;
+
+    echo "Operating on $(basename $d)..."
+    merge_trace=$coverage_dir/$(basename $d)-check.trace
+    touch $merge_trace
+    echo "  merging $(basename $d) traces in $merge_trace..."
+    for f in `find $d -name posix*.trace`;
+    do
+        # echo "  merging $f in $merge_trace"
+        echo "" >> $f
+        cat $f >> $merge_trace
+    done;
+    echo ""
+    echo "  checker is running on $merge_trace"
+    $check_command -arch $architecture $merge_trace
+    mv bisect0001.out $coverage_dir/$(basename $d)-report.out
+done;
+
+cd ./src_ext/fs/
+echo "Running bisect-report on partial reports..."
+bisect-report -html $coverage_dir/total_coverage_html `find $coverage_dir -name *-report.out`
+
+echo "All the outputs of this script are in:
+
+      $coverage_dir
+
+      The html report is consultable running (substitute to
+      firefox your preferred web browser):
+
+      firefox $coverage_dir/total_coverage_html/index.html"
